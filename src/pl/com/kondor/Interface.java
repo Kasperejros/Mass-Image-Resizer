86d61df4691ca862976ba933ec363c2a95eeed11
Kasperejros <krajewski.kacper@gmail.com>
2018-06-29 15:06:46 +0200

    added param fields to interface

--- src/pl/com/kondor/Interface.java
+++ src/pl/com/kondor/Interface.java
@@ -13,6 +13,7 @@ import javax.swing.DefaultComboBoxModel;
 import java.awt.Font;
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 
 public class Interface {
 
@@ -20,6 +21,9 @@ public class Interface {
 	private JTextArea logText = new JTextArea();
 	private ResizeManager resizeManager = new ResizeManager();
 	private String[] resizeNames = resizeManager.nameList();
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -50,8 +54,9 @@ public class Interface {
 	@SuppressWarnings({ "rawtypes", "unchecked" })
 	private void initialize() {
 		frmMassImageResizer = new JFrame();
+		frmMassImageResizer.setResizable(false);
 		frmMassImageResizer.setTitle("Mass Image Resizer");
-		frmMassImageResizer.setBounds(100, 100, 450, 300);
+		frmMassImageResizer.setBounds(100, 100, 450, 360);
 		frmMassImageResizer.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmMassImageResizer.getContentPane().setLayout(null);
 		Chooser chooser = new Chooser(this);
@@ -70,15 +75,15 @@ public class Interface {
 		
 		JSeparator separator_1 = new JSeparator();
 		separator_1.setOrientation(SwingConstants.VERTICAL);
-		separator_1.setBounds(216, 99, 2, 50);
+		separator_1.setBounds(216, 99, 2, 106);
 		frmMassImageResizer.getContentPane().add(separator_1);
 		
 		JSeparator separator_2 = new JSeparator();
-		separator_2.setBounds(10, 160, 414, 2);
+		separator_2.setBounds(10, 216, 414, 2);
 		frmMassImageResizer.getContentPane().add(separator_2);
 		
 		JLabel lblFormaResizeu = new JLabel("Forma Zmiany Rozmiaru:");
-		lblFormaResizeu.setBounds(10, 99, 196, 14);
+		lblFormaResizeu.setBounds(20, 99, 186, 14);
 		frmMassImageResizer.getContentPane().add(lblFormaResizeu);
 		
 		JComboBox resizeForm = new JComboBox();
@@ -96,19 +101,19 @@ public class Interface {
 		frmMassImageResizer.getContentPane().add(saveForm);
 		
 		JButton startButton = new JButton("Uruchom");
-		startButton.setBounds(20, 181, 186, 50);
+		startButton.setBounds(20, 245, 186, 50);
 		frmMassImageResizer.getContentPane().add(startButton);
 		
 		JLabel lblLog = new JLabel("Log:");
-		lblLog.setBounds(216, 167, 46, 14);
+		lblLog.setBounds(216, 231, 46, 14);
 		frmMassImageResizer.getContentPane().add(lblLog);
 		
 		JButton copyLogButton = new JButton("Kopiuj do schowka");
-		copyLogButton.setBounds(216, 236, 198, 14);
+		copyLogButton.setBounds(216, 300, 198, 14);
 		frmMassImageResizer.getContentPane().add(copyLogButton);
 		
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(216, 181, 198, 50);
+		scrollPane.setBounds(216, 245, 198, 50);
 		frmMassImageResizer.getContentPane().add(scrollPane);
 		scrollPane.setViewportView(logText);
 		
@@ -119,6 +124,35 @@ public class Interface {
 		logText.setForeground(Color.GREEN);
 		logText.setBackground(Color.BLACK);
 		logText.setFont(new Font("Tahoma", Font.PLAIN, 10));
+		
+		textField = new JTextField();
+		textField.setBounds(103, 154, 73, 20);
+		frmMassImageResizer.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setEditable(false);
+		textField_1.setEnabled(false);
+		textField_1.setColumns(10);
+		textField_1.setBounds(103, 185, 73, 20);
+		frmMassImageResizer.getContentPane().add(textField_1);
+		
+		JLabel lblNewLabel = new JLabel("Procent Skali:");
+		lblNewLabel.setBounds(20, 158, 73, 14);
+		frmMassImageResizer.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNieaktywne = new JLabel("Nieaktywne:");
+		lblNieaktywne.setBounds(21, 188, 73, 14);
+		frmMassImageResizer.getContentPane().add(lblNieaktywne);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(314, 168, 100, 20);
+		frmMassImageResizer.getContentPane().add(textField_2);
+		textField_2.setColumns(10);
+		
+		JLabel lblPrefix = new JLabel("Sufix:");
+		lblPrefix.setBounds(230, 171, 73, 14);
+		frmMassImageResizer.getContentPane().add(lblPrefix);
 	}
 	
 	public void logWrite(String line) {
